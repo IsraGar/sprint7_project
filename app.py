b5cad6381b447ff827722bd93e838136c896df38
IsraGar <isragarciahdez@gmail.com>
2025-07-16 10:14:33 -0600

    Refactorización de archivo app.py. Se agrega gráfico de barras a la app.

--- app.py
+++ app.py
@@ -5,15 +5,28 @@ import plotly.express as px
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
 st.header('Sprint-7 - Project')
-#1er diagrama
-build_histogram = st.checkbox('Construir un histograma')
-build_scatter = st.checkbox('Construir diagrama de dispersión')
-if build_histogram:
+grafica = st.radio(
+    "Selecciona el tipo de gráfico que deseas visualizar:",
+    options=["Ninguno", "Histograma", "Dispersión", "Barras"]
+)
+
+if grafica == "Histograma":
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')         
     fig = px.histogram(car_data, x="odometer")     
     st.plotly_chart(fig, use_container_width=True)
-elif build_scatter:
+
+elif grafica == "Dispersión":
     st.write('Creación de diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x="odometer", y="price")
-    st.ploty_chart(fig, use_container_width=True)
-    
+    st.plotly_chart(fig, use_container_width=True)
+
+elif grafica == "Barras":
+    st.write('Creación de gráfico de barras de Top 5 de tipos de coches por precio')
+    car_data_top_5 = (
+    car_data.groupby('type', as_index=False)['price']
+    .sum()
+    .sort_values(by='price', ascending=False)
+    .head(5)
+    )
+    fig = px.bar(car_data_top_5, x="type", y="price", title="Top 5 tipos de coches por precio total") # crear un gráfico de dispersión
+    st.plotly_chart(fig, use_container_width=True)
